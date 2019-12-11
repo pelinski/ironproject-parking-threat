dbe3e88abbfe0dbc8ce6914135c993d5ec90605d
pelinski <teresapelinski@gmail.com>
2019-12-11 20:03:21 +0100

    render void works

--- js/game.js
+++ js/game.js
@@ -9,6 +9,38 @@ class Game {
   init() {
     this.player = new Player(this.ctx);
     this.player.draw();
+    this.update();
+  }
+
+  update() {
+    let past = 0;
+    let delta = 0;
+
+    function draw(elapsed) {
+      delta = elapsed - past;
+      past = elapsed;
+      let fps = 1000 / delta;
+      this.ctx.clearRect(0, 0, 500, 500);
+
+      /*
+
+      // Update game objects
+      gameObjects.forEach(function(object) {
+        object.update(delta);
+      });*/
+
+      this.ctx.save();
+      this.player.draw();
+      console.log("update");
+      this.ctx.restore();
+
+      // Draw fps counter
+      this.ctx.font = "20px Arial";
+      this.ctx.fillStyle = "black";
+      this.ctx.fillText("FPS: " + Math.round(fps), 10, 20);
+      window.requestAnimationFrame(draw.bind(this));
+    }
+    window.requestAnimationFrame(draw.bind(this));
   }
 
   /* 
