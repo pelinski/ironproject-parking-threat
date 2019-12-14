9b7e1394e5cf11083bb37604cc6339b5e5c267a9
pelinski <teresapelinski@gmail.com>
2019-12-14 18:56:00 +0100

    player child class working

--- js/player.js
+++ js/player.js
@@ -1,16 +1,12 @@
-class Player {
-  constructor(
-    ctx,
-    startPosX = 100,
-    startPosY = 100,
-    width = 100,
-    height = 150
-  ) {
-    this.ctx = ctx;
-    this.width = width;
-    this.height = height;
-    this.posX = startPosX;
-    this.posY = startPosY;
+class Player extends Car {
+  constructor(ctx, startPosX, startPosY, width, height) {
+    super(
+      ctx,
+      (startPosX = 100),
+      (startPosY = 100),
+      (width = 100),
+      (height = 150)
+    );
     this.speed = 0; //px por segundo;
     this.rotationDir = 0;
     this.angle = 0;
@@ -21,22 +17,6 @@ class Player {
       RIGHT: 39
     };
     this.setListeners();
-    this.image = this.chargeImage();
-  }
-
-  draw() {
-    this.ctx.save();
-    this.ctx.translate(this.posX + this.width / 2, this.posY + this.height / 2);
-    this.ctx.rotate(-this.rads());
-    this.ctx.drawImage(
-      this.image,
-      -this.width / 2,
-      -this.height / 2,
-      this.width,
-      this.height
-    );
-
-    this.ctx.restore();
   }
 
   update(delta) {
@@ -75,29 +55,5 @@ class Player {
 
   rads() {
     return (this.angle * 2 * Math.PI) / 360;
-  }
-
-  // methods for graphics
-
-  chargeImage() {
-    let image = new Image();
-    image.src = this.getRandomCarImage();
-    return image;
-  }
-
-  getRandomCarImage() {
-    let color = ["blue", "green", "orange", "purple", "red", "yellow"];
-    let src =
-      "./images/parking-pack/PNG/Cars/car" +
-      this.getRandomInt(1, 7).toString() +
-      "_" +
-      color[this.getRandomInt(0, color.length - 1)] +
-      ".png";
-    console.log(src);
-    return src;
-  }
-
-  getRandomInt(start, end) {
-    return Math.floor(Math.random() * (end - start)) + start;
   }
 }
