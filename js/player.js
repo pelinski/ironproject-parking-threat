5361cd0f678bef33c10ecd7769432d70549f00f4
pelinski <teresapelinski@gmail.com>
2019-12-17 20:07:24 +0100

    bad

--- js/player.js
+++ js/player.js
@@ -22,7 +22,7 @@ class Player extends Car {
     this.carCenter = [this.posX + this.width / 2, this.posY + this.height / 2];
     this.speed = 0; //px por segundo;
     this.rotationDir = 0;
-    this.angle = 0;
+    this.angle = -90; // car facing up
     this.keyMap = {
       UP: 38,
       DOWN: 40,
@@ -35,8 +35,8 @@ class Player extends Car {
 
   draw() {
     this.ctx.save();
-    this.ctx.translate(this.posX + this.width / 2, this.posY + this.height / 2);
-    this.ctx.rotate(-this.rads());
+    this.ctx.translate(this.carCenter[0], this.carCenter[1]);
+    this.ctx.rotate(this.rads() + Math.PI / 2);
     this.ctx.drawImage(
       this.image,
       -this.width / 2,
@@ -46,44 +46,32 @@ class Player extends Car {
     );
     this.ctx.restore();
     this.ctx.beginPath();
-    this.ctx.rect(this.carVertexAbs.A[0],this.carVertexAbs.A[1],this.width,this.height);
+    this.ctx.rect(
+      //center
+      this.posX + this.width / 2,
+      this.posY + this.height / 2,
+      5,
+      5
+    ); //control vertices
+    this.ctx.rect(this.carVertexAbs.A[1], this.carVertexAbs.A[0], 5, 5);
     this.ctx.stroke();
   }
 
   update(delta, obs) {
-    
     if (
-      //there is a collision
-      obs.some(
-        function(obs) {
-          return (
-            (this.carVertexAbs.A[0] + this.width > obs.posX &&
-              obs.posX + obs.width > this.carVertexAbs.A[0] &&
-              this.carVertexAbs.A[1] + this.height > obs.posY &&
-              obs.posY + obs.height > this.carVertexAbs.A[1]) ||
-            (this.carVertexAbs.B[0] + this.width > obs.posX &&
-              obs.posX + obs.width > this.carVertexAbs.B[0] &&
-              this.carVertexAbs.B[1] + this.height > obs.posY &&
-              obs.posY + obs.height > this.carVertexAbs.B[1]) ||
-            (this.carVertexAbs.C[0] + this.width > obs.posX &&
-              obs.posX + obs.width > this.carVertexAbs.C[0] &&
-              this.carVertexAbs.C[1] + this.height > obs.posY &&
-              obs.posY + obs.height > this.carVertexAbs.C[1]) ||
-            (this.carVertexAbs.D[0] + this.width > obs.posX &&
-              obs.posX + obs.width > this.carVertexAbs.D[0] &&
-              this.carVertexAbs.D[1] + this.height > obs.posY &&
-              obs.posY + obs.height > this.carVertexAbs.D[1])
-          );
-        }.bind(this)
-      )
+      this.carVertexAbs.A[0] > 50 &&
+      this.carVertexAbs.A[0] < 300 &&
+      this.carVertexAbs.A[1] < 100 + 150 &&
+      this.carVertexAbs.A[0] > 100
     ) {
-      console.error("in")
-      this.posX -= 0.5 * Math.sin(this.rads()) * (this.speed / 1000) * delta;
-      this.posY -= 0.5 * Math.cos(this.rads()) * (this.speed / 1000) * delta;
+      console.error("in");
+      this.posX -= 0.5 * Math.cos(this.rads()) * (this.speed / 1000) * delta;
+      this.posY -= 0.5 * Math.sin(this.rads()) * (this.speed / 1000) * delta;
     } else {
       this.move(delta);
     }
 
+    this.positions();
     // do not get out of the canvas
 
     /*if (this.posX > 500) {
@@ -102,20 +90,12 @@ class Player extends Car {
   }
 
   move(delta) {
-    this.posX += Math.sin(this.rads()) * (this.speed / 1000) * delta;
-    this.posY += Math.cos(this.rads()) * (this.speed / 1000) * delta;
-
-    //debug control
-    this.positions();
+    this.posX += Math.cos(this.rads()) * (this.speed / 1000) * delta;
+    this.posY += Math.sin(this.rads()) * (this.speed / 1000) * delta;
   }
 
   positions() {
     //TODO : simplify this sintax
-
-    this.carVertex.A = this.vertexRot(this.carVertex.A);
-    this.carVertex.B = this.vertexRot(this.carVertex.B);
-    this.carVertex.C = this.vertexRot(this.carVertex.C);
-    this.carVertex.D = this.vertexRot(this.carVertex.D);
 
     this.carVertexAbs.A = this.sumVectors(this.carVertex.A, this.carCenter);
     this.carVertexAbs.B = this.sumVectors(this.carVertex.B, this.carCenter);
@@ -142,6 +122,15 @@ class Player extends Car {
     */
   }
 
+  rotateAbs(type) {
+    if (type != 0) {
+      this.carVertex.A = this.vertexRot(this.carVertex.A);
+      this.carVertex.B = this.vertexRot(this.carVertex.B);
+      this.carVertex.C = this.vertexRot(this.carVertex.C);
+      this.carVertex.D = this.vertexRot(this.carVertex.D);
+    }
+  }
+
   setListeners() {
     document.addEventListener("keydown", this.eventHandler(1).bind(this));
     document.addEventListener("keyup", this.eventHandler(0).bind(this));
@@ -151,13 +140,15 @@ class Player extends Car {
     return function(event) {
       event.preventDefault(); //prevent page from scrolling
       if (event.keyCode === this.keyMap.UP) {
-        this.speed = -100 * type;
+        this.speed = 100 * type;
       } else if (event.keyCode === this.keyMap.DOWN) {
-        this.speed = +100 * type;
+        this.speed = -100 * type;
       } else if (event.keyCode === this.keyMap.RIGHT) {
-        this.rotationDir = 1 * type;
-      } else if (event.keyCode === this.keyMap.LEFT) {
         this.rotationDir = -1 * type;
+        this.rotateAbs(type);
+      } else if (event.keyCode === this.keyMap.LEFT) {
+        this.rotationDir = 1 * type;
+        this.rotateAbs(type);
       }
     };
   }
@@ -175,6 +166,9 @@ class Player extends Car {
 
     At[0] = rotationMatrix[0][0] * A[0] + rotationMatrix[0][1] * A[1];
     At[1] = rotationMatrix[1][0] * A[0] + rotationMatrix[1][1] * A[1];
+
+
+    At[1] = -At[1]; 
 
     return At;
   }
